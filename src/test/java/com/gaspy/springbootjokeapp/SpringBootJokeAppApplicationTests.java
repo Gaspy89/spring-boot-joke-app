51fded22a623b87a41c206adccfa3636ea38e7ed
Marton Gaspardy <gaspy89@gmail.com>
2020-12-03 10:23:54 +0100

    init project

---
+++ src/test/java/com/gaspy/springbootjokeapp/SpringBootJokeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaspy.springbootjokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJokeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
